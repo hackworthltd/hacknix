cae2466c32f580863b1abe0b588a35d5827873a1
Drew Hess <dhess-src@hackworthltd.com>
2020-04-10 14:18:32 +0100

    Re-enable the Hydra nix-prefetch-git patches.
    This is needed for projects with submodules.

--- overlays/patches.nix
+++ overlays/patches.nix
@@ -20,24 +20,25 @@ in
 
 
   # Hydra fixes from iohk.
-  # hydra-unstable = super.hydra-unstable.overrideAttrs (drv: {
-  #   patches = [
-  #     (super.fetchpatch {
-  #       url = "https://github.com/input-output-hk/hydra/commit/ed87d2ba82807d30d91d70a88cda276083ef4e79.patch";
-  #       sha256 = "0mzmm480cs085wbbn1ha6ml164v0dslfh0viak73mc84rvl00ckb";
-  #     })
-  #     (super.fetchpatch {
-  #       url = "https://github.com/input-output-hk/hydra/commit/96ec35acface848c546b67e6b835094412a488d9.patch";
-  #       sha256 = "0ax4bn1ivk5yg5mbdkcqbqc2vrjlyk9km2rzg1k1kqc81ka570a2";
-  #     })
-  #     (super.fetchpatch {
-  #       url = "https://github.com/input-output-hk/hydra/commit/11db34b6a9243a428b3d5935c65ac13a8080d02c.patch";
-  #       sha256 = "10b9ywif14gncf1j9647hhpbqh67yy0pv1c8f38f08v68a83993s";
-  #     })
-  #     (super.fetchpatch {
-  #       url = "https://github.com/input-output-hk/hydra/commit/0768891e3cd3ef067d28742098f1dea8462fca75.patch";
-  #       sha256 = "0m4009pmi4sl0vwq6q98bzp4hpnfr4ww1j27czwcazbda7l8fdzy";
-  #     })
-  #   ];
-  # });
+  hydra-unstable = super.hydra-unstable.overrideAttrs (drv: {
+    patches = [
+      (super.fetchpatch {
+        url = "https://github.com/input-output-hk/hydra/commit/ed87d2ba82807d30d91d70a88cda276083ef4e79.patch";
+        sha256 = "0mzmm480cs085wbbn1ha6ml164v0dslfh0viak73mc84rvl00ckb";
+      })
+      (super.fetchpatch {
+        url = "https://github.com/input-output-hk/hydra/commit/96ec35acface848c546b67e6b835094412a488d9.patch";
+        sha256 = "0ax4bn1ivk5yg5mbdkcqbqc2vrjlyk9km2rzg1k1kqc81ka570a2";
+      })
+      (super.fetchpatch {
+        url = "https://github.com/input-output-hk/hydra/commit/0768891e3cd3ef067d28742098f1dea8462fca75.patch";
+        sha256 = "0m4009pmi4sl0vwq6q98bzp4hpnfr4ww1j27czwcazbda7l8fdzy";
+      })
+      # Broken on hydra-unstable.
+      # (super.fetchpatch {
+      #   url = "https://github.com/input-output-hk/hydra/commit/11db34b6a9243a428b3d5935c65ac13a8080d02c.patch";
+      #   sha256 = "10b9ywif14gncf1j9647hhpbqh67yy0pv1c8f38f08v68a83993s";
+      # })
+    ];
+  });
 }
