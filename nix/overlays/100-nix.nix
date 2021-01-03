16e450718fef92bbc51ecc3fddaf80245dac45a6
Drew Hess <dhess-src@hackworthltd.com>
2021-01-03 18:06:08 +0000

    Another attempt to fix our nix patch.

--- nix/overlays/100-nix.nix
+++ nix/overlays/100-nix.nix
@@ -1,9 +1,21 @@
 final: prev:
 let
   nixUnstable = prev.nixUnstable.overrideAttrs (drv: {
-    patches = (drv.patches or [ ]) ++ [
-      # Work around nix issue; see:
-      # https://github.com/NixOS/nix/pull/4264
+    # This patch works around a Boehm GC issue; see
+    # https://github.com/NixOS/nix/pull/4264
+    #
+    # Note: patches don't compose well in overlays. If we try to add
+    # the patch to the list of prev's patches, then including this
+    # overlay in a list of overlays where any of the previous overlays
+    # also included it causes the patch to be applied again.
+    #
+    # Trying to sort and unique the list also doesn't work, because we
+    # can't compare attrsets, and each individual patch in the list is
+    # an attrset. (We could try to hash the patch attrsets or
+    # something like that.)
+    #
+    # Therefore, we just override prev's patches. This is gross.
+    patches = [
       (prev.fetchpatch {
         url = "https://github.com/NixOS/nix/pull/4264.patch";
         sha256 = "sha256-pEvymyZLidu1Zn5LpRXcfRXF/cNqglDLx2HIGrbwksE=";
