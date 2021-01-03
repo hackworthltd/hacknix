7df88e736cf9802e8a0602c40dcbebaedb6ac42a
Drew Hess <dhess-src@hackworthltd.com>
2021-01-03 18:02:13 +0000

    Revert "Patches don't compose well in overlays."
    This reverts commit ffa7642f662ab61c6e7337ae5bd2e304a94870e3.
    
    Nix (& nixpkgs) doesn't support comparing attrsets.

--- nix/overlays/100-nix.nix
+++ nix/overlays/100-nix.nix
@@ -1,19 +1,14 @@
 final: prev:
 let
   nixUnstable = prev.nixUnstable.overrideAttrs (drv: {
-    # Note: patches don't compose well in overlays -- if we try to add
-    # this patch to drv.patches, then every time we include this
-    # overlay in a subsequent overlay, it'll try to patch it again.
-    # Therefore, we sort and unique-ify the list. :\
-
-    patches = prev.lib.unique (prev.lib.sort (x: y: x < y) ((drv.patches or [ ]) ++ [
+    patches = (drv.patches or [ ]) ++ [
       # Work around nix issue; see:
       # https://github.com/NixOS/nix/pull/4264
       (prev.fetchpatch {
         url = "https://github.com/NixOS/nix/pull/4264.patch";
         sha256 = "sha256-pEvymyZLidu1Zn5LpRXcfRXF/cNqglDLx2HIGrbwksE=";
       })
-    ]));
+    ];
   });
   nixFlakes = nixUnstable;
 
