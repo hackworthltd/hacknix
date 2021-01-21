06aef53934542bd87c71d1d55fb47d5f1976569d
Drew Hess <dhess-src@hackworthltd.com>
2021-01-21 21:32:19 +0000

    Drop most Hydra patches.
    It appears the git submodule issues have been resolved upstream; see:
    
    https://github.com/dhall-lang/dhall-haskell/issues/766
    
    We also probably don't need the hydra-no-restrict patch anymore, so
    long as we add this to the NixOS Hydra module config:
    
    https://github.com/cleverca22/nixos-configs/blob/7322b81f4fa7271c015014ed44595030f700b862/nas-hydra.nix#L32-L34
    
    See https://github.com/NixOS/nix/issues/1888#issuecomment-562311421
    
    We keep the secure GitHub token-handling patch, and add a patch which
    enables verbose mode when `hydra-eval-jobs` runs, for better logging
    and some visibility into what Hydra is up to during evaluations.

--- overlays/patches.nix
+++ overlays/patches.nix
@@ -1,33 +1,9 @@
 self: super:
 let
-
-in
-{
-  # Hydra fixes from iohk and other various sources.
+  # Hydra fixes from various sources.
   hydra-unstable = super.hydra-unstable.overrideAttrs (
     drv: {
       patches = [
-        (
-          super.fetchpatch {
-            url =
-              "https://github.com/input-output-hk/hydra/commit/ed87d2ba82807d30d91d70a88cda276083ef4e79.patch";
-            sha256 = "0mzmm480cs085wbbn1ha6ml164v0dslfh0viak73mc84rvl00ckb";
-          }
-        )
-        (
-          super.fetchpatch {
-            url =
-              "https://github.com/input-output-hk/hydra/commit/96ec35acface848c546b67e6b835094412a488d9.patch";
-            sha256 = "0ax4bn1ivk5yg5mbdkcqbqc2vrjlyk9km2rzg1k1kqc81ka570a2";
-          }
-        )
-        (
-          super.fetchpatch {
-            url =
-              "https://github.com/input-output-hk/hydra/commit/0768891e3cd3ef067d28742098f1dea8462fca75.patch";
-            sha256 = "0m4009pmi4sl0vwq6q98bzp4hpnfr4ww1j27czwcazbda7l8fdzy";
-          }
-        )
         # Secure GitHub token handling.
         (
           super.fetchpatch {
@@ -37,19 +13,14 @@ in
           }
         )
 
-        # Disable restrictEval until we can work out the gitignoreSource issue.
-        ../patches/hydra/hydra-no-restrict.patch
-        # (super.fetchpatch {
-        #   url =
-        #     "https://github.com/NixOS/hydra/commit/2f9d422172235297759f2b224fe0636cad07b6fb.patch";
-        #   sha256 = "0152nsqqc5d85qdygmwrsk88i9y6nk1b639fj2n042pjvr0kpz6k";
-        # })
-        # Broken on hydra-unstable.
-        # (super.fetchpatch {
-        #   url = "https://github.com/input-output-hk/hydra/commit/11db34b6a9243a428b3d5935c65ac13a8080d02c.patch";
-        #   sha256 = "10b9ywif14gncf1j9647hhpbqh67yy0pv1c8f38f08v68a83993s";
-        # })
+        # Enable verbose mode in hydra-eval-jobs so that we can see
+        # some semblance of progress in the logs.
+        ../patches/hydra/verbose-hydra-eval-jobs.patch
       ];
     }
   );
+
+in
+{
+  inherit hydra-unstable;
 }
