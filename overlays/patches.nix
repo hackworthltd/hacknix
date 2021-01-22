b17c86b94851a61012e43730fc237714777991d4
Drew Hess <dhess-src@hackworthltd.com>
2021-01-22 13:35:51 +0000

    Attempt to work around a recent Nix etags issue we're having.
    See:
    https://github.com/NixOS/nix/issues/4469
    
    Here, rather than asserting when this condition occurs, we simply
    output a warning, and provide the URL that generated the problem and
    the expected/actual etags.

--- overlays/patches.nix
+++ overlays/patches.nix
@@ -24,7 +24,28 @@ let
     }
   );
 
+  # Try to work around https://github.com/NixOS/nix/issues/4469, and
+  # also log info when it occurs.
+  nixUnstable = super.nixUnstable.overrideAttrs (
+    drv: {
+      patches = [
+
+        # Replicate the patch from upstream, since patches aren't
+        # composable via overlays :(
+        (super.fetchpatch {
+          # Fix build on gcc10
+          url = "https://github.com/NixOS/nix/commit/d4870462f8f539adeaa6dca476aff6f1f31e1981.patch";
+          sha256 = "mTvLvuxb2QVybRDgntKMq+b6da/s3YgM/ll2rWBeY/Y=";
+        })
+
+        ../patches/nix/etag.patch
+      ];
+    }
+  );
+  nixFlakes = nixUnstable;
+
 in
 {
   inherit hydra-unstable;
+  inherit nixUnstable nixFlakes;
 }
