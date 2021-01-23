f6e261c920461a6d37579d5536dc46924f6926dc
Drew Hess <dhess-src@hackworthltd.com>
2021-01-23 13:51:06 +0000

    fixup! Use upstream Hydra as an overlay so it picks up our patched Nix.

--- overlays/patches.nix
+++ overlays/patches.nix
@@ -4,6 +4,14 @@ let
   hydra-unstable = super.hydra-unstable.overrideAttrs (
     drv: {
       patches = [
+        # Fix for latest nixUnstable.
+        (
+          super.fetchpatch {
+            url = "https://github.com/NixOS/hydra/pull/840.diff";
+            sha256 = "sha256-KPc1q36Mi/aJvm3n8ZxIdMHZwLNYnhz3NRsCR/iEtqU=";
+          }
+        )
+
         # Secure GitHub token handling.
         (
           super.fetchpatch {
