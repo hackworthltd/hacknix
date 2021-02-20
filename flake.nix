60e830d0aca5d8b3795c75f0837c3f72ff7a9168
Drew Hess <dhess-src@hackworthltd.com>
2021-02-20 14:39:25 +0000

    Follow our nixpkgs for until upstream Python watchdog is fixed.
    See https://github.com/NixOS/nixpkgs/issues/113777

--- flake.nix
+++ flake.nix
@@ -2,7 +2,11 @@
   description = "Hackworth Ltd's nixpkgs overlays and NixOS modules.";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    #nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+
+    # Follow our fork until
+    # https://github.com/NixOS/nixpkgs/issues/113777 is resolved.
+    nixpkgs.url = github:hackworthltd/nixpkgs/fix-python-watchdog;
 
     nix-darwin.url = github:LnL7/nix-darwin;
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
