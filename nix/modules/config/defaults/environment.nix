cdc0f8078fe573371355bad101420791f90575f7
Drew Hess <dhess-src@hackworthltd.com>
2021-03-21 18:35:13 +0000

    Remove environment.shellAliases from hacknix defaults.

--- nix/modules/config/defaults/environment.nix
+++ nix/modules/config/defaults/environment.nix
@@ -12,19 +12,7 @@ in
   };
 
   config = mkIf enabled {
-
     environment.systemPackages = with pkgs; [ emacs-nox git wget ];
-
-    environment.shellAliases = {
-      l = "ls -F";
-      ll = "ls -alF";
-      ls = "ls -F";
-      ltr = "ls -alFtr";
-      m = "more";
-      more = "less";
-      mroe = "less";
-      pfind = "ps auxww | grep";
-    };
 
     # Disable HISTFILE globally.
     environment.interactiveShellInit = ''
@@ -32,7 +20,5 @@ in
     '';
 
     programs.bash.enableCompletion = true;
-
   };
-
 }
