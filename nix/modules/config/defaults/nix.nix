95504eb5f6499897c659d8d385d1c37b9b136735
Drew Hess <dhess-src@hackworthltd.com>
2021-03-21 18:35:38 +0000

    Make it easier to disable Nix flakes in hacknix defaults.
    Also, remove some antiquated bits from the NixOS system configuration.

--- nix/modules/config/defaults/nix.nix
+++ nix/modules/config/defaults/nix.nix
@@ -1,55 +1,25 @@
 { config, pkgs, lib, ... }:
 
-with lib;
 let
   cfg = config.hacknix.defaults.nix;
-  enabled = cfg.enable;
 in
 {
   options.hacknix.defaults.nix = {
-    enable = mkEnableOption "the hacknix Nix configuration defaults.";
+    enable = lib.mkEnableOption ''
+      the hacknix Nix configuration defaults.
+
+      At the moment, all this option does is enable Nix flakes support
+      for the system Nix version.
+    '';
   };
 
-  config = mkIf enabled {
-
-    nixpkgs.config.allowUnfree = true;
-
-    # Most of the following config is thanks to Graham Christensen,
-    # from:
-    # https://github.com/grahamc/network/blob/1d73f673b05a7f976d82ae0e0e61a65d045b3704/modules/standard/default.nix#L56
-    #
-    # XXX dhess - now that we're using flakes, some of this is no
-    # longer needed. Do a cleanup pass.
-
+  config = lib.mkIf cfg.enable {
+    # Enable flakes support.
     nix = {
-      # Enable flakes support.
       package = pkgs.nixFlakes;
       extraOptions = ''
         experimental-features = nix-command flakes
       '';
-
-      useSandbox = true;
-      nixPath = [
-        (
-          let
-            cfg = pkgs.writeText "configuration.nix" ''
-              assert builtins.trace "This server is managed by NixOps; do not run `nixos-rebuild` here." false;
-              {}
-            '';
-          in
-          "nixos-config=${cfg}"
-        )
-
-        # Copy the channel version from the deploy host to the target
-        "nixpkgs=/run/current-system/nixpkgs"
-      ];
     };
-
-    system.extraSystemBuilderCmds = ''
-      ln -sv ${pkgs.path} $out/nixpkgs
-    '';
-    environment.etc.host-nix-channel.source = pkgs.path;
-
   };
-
 }
