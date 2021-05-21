018bc4dbaeab0d23e683deb3b9d717cfbb6e8f49
Drew Hess <src@drewhess.com>
2021-05-21 23:22:03 +0100

    Add a Vault plugins helper.

--- flake.nix
+++ flake.nix
@@ -141,6 +141,7 @@
               inherit (pkgs) ffdhe2048Pem ffdhe3072Pem ffdhe4096Pem;
 
               inherit (pkgs) vault-plugin-secrets-github;
+              inherit (pkgs) vault-plugins register-vault-plugins;
 
               # From sops-nix.
               inherit (pkgs) sops-init-gpg-key sops-install-secrets sops-pgp-hook ssh-to-pgp;
--- nix/overlays/500-vault-plugins.nix
+++ nix/overlays/500-vault-plugins.nix
@@ -1,7 +1,9 @@
 final: prev:
 let
   vault-plugin-secrets-github = final.callPackage ../pkgs/vault-plugin-secrets-github { };
+  vault-plugins = final.callPackage ../pkgs/vault-plugins { };
 in
 {
   inherit vault-plugin-secrets-github;
+  inherit (vault-plugins) vault-plugins register-vault-plugins;
 }
