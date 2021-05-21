d3c4b3bdc78776365133467ad9956e70ba1865ad
Drew Hess <src@drewhess.com>
2021-05-21 17:11:13 +0100

    Remove the "key" submodule/type.
    We're no longer using NixOps and therefore don't need this type
    anymore.

--- nix/overlays/131-lib-more-types.nix
+++ nix/overlays/131-lib-more-types.nix
@@ -1,87 +1,7 @@
 # Additional useful types, mostly for NixOS modules.
 
-# The key type defined here is based on keyOptionType in NixOps. As it
-# is a derivative work of NixOps, it is covered by the GNU LGPL; see
-# the LICENSE file included with this source distribution.
-
 final: prev:
 let
-  ## A key type for configuring secrets that are stored in the
-  ## filesystem. The option names and types here are compatible with
-  ## NixOps's `keyType`, so they can be mechanically mapped to
-  ## `deployment.keys`, but there are a few differences; namely, this
-  ## type ensures that its paths are not contained in the Nix store,
-  ## so that the chances of accidentally storing a secret in the store
-  ## are minimized.
-  key = final.lib.types.submodule
-    (
-      { config, name, ... }: {
-        options.text = final.lib.mkOption {
-          example = "secret stuff";
-          type = final.lib.types.nonEmptyStr;
-          description = ''
-            This designates the text that the key should contain. So if
-            the key name is <replaceable>password</replaceable> and
-            <literal>foobar</literal> is set here, the contents of the
-            file
-            <filename><replaceable>destDir</replaceable>/<replaceable>password</replaceable></filename>
-            will be <literal>foobar</literal>.
-          '';
-        };
-
-        options.destDir = final.lib.mkOption {
-          default = "/run/keys";
-          type = final.lib.types.nonStorePath;
-          description = ''
-            When specified, this allows changing the destDir directory of the key
-            file from its default value of <filename>/run/keys</filename>.
-
-            This directory will be created, its permissions changed to
-            <literal>0750</literal> and ownership to <literal>root:keys</literal>.
-          '';
-        };
-
-        options.path = final.lib.mkOption {
-          type = final.lib.types.nonStorePath;
-          default = "${config.destDir}/${name}";
-          internal = true;
-          description = ''
-            Path to the destination of the file, a shortcut to
-            <literal>destDir</literal> + / + <literal>name</literal>
-
-            Example: For key named <literal>foo</literal>,
-            this option would have the value <literal>/run/keys/foo</literal>.
-          '';
-        };
-
-        options.user = final.lib.mkOption {
-          default = "root";
-          type = final.lib.types.nonEmptyStr;
-          description = ''
-            The user which will be the owner of the key file.
-          '';
-        };
-
-        options.group = final.lib.mkOption {
-          default = "root";
-          type = final.lib.types.nonEmptyStr;
-          description = ''
-            The group that will be set for the key file.
-          '';
-        };
-
-        options.permissions = final.lib.mkOption {
-          default = "0400";
-          example = "0640";
-          type = final.lib.types.nonEmptyStr;
-          description = ''
-            The default permissions to set for the key file, needs to be in the
-            format accepted by <citerefentry><refentrytitle>chmod</refentrytitle>
-            <manvolnum>1</manvolnum></citerefentry>.
-          '';
-        };
-      }
-    );
   fwRule = final.lib.types.listOf
     (
       final.lib.types.submodule {
@@ -647,7 +567,6 @@ in
 {
   lib = (prev.lib or { }) // {
     types = (prev.lib.types or { }) // {
-      inherit key;
       inherit fwRule fwRule6;
       inherit ipv4Subnet dhcp4Subnet;
       inherit ipv6Subnet;
