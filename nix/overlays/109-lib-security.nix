84efff551b02649af88927a990124b523293a667
Drew Hess <src@drewhess.com>
2021-05-21 17:03:10 +0100

    Update sslModernCiphers.

--- nix/overlays/109-lib-security.nix
+++ nix/overlays/109-lib-security.nix
@@ -57,9 +57,14 @@ let
   # to harden the ciphers that the program will accept.
   #
   # From:
-  # https://mozilla.github.io/server-side-tls/ssl-config-generator/?server=nginx-1.10.3&openssl=1.0.1e&hsts=yes&profile=modern
+  # https://ssl-config.mozilla.org/#server=nginx&version=1.17.7&config=intermediate&openssl=1.1.1d&guideline=5.6
+  #
+  # Note: the "modern" Mozilla OpenSSL config now recommends simply
+  # setting ssl_protocols to TLSv1.3 and leaving ssl_ciphers
+  # unspecified, but that might be too aggressive in some cases. In
+  # any case, this setting should probably only be used with TLSv1.2.
 
-  sslModernCiphers = "ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256";
+  sslModernCiphers = "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384";
 in
 {
   lib = (prev.lib or { }) // {
