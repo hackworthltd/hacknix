a59416eb80052b3898bcdd614aa1c04b059eb817
Drew Hess <src@drewhess.com>
2021-05-21 21:46:30 +0100

    Add a note about why we need the aws-sam-cli override.

--- nix/overlays/500-aws.nix
+++ nix/overlays/500-aws.nix
@@ -1,5 +1,8 @@
 final: prev:
 let
+  # Note: we use an older version than upstream because
+  # python-watchdog is broken upstream. See
+  # https://github.com/NixOS/nixpkgs/issues/113777
   aws-sam-cli = final.callPackage ../pkgs/aws-sam-cli { };
 
 in
