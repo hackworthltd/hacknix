9db90065e7b1036148a9fb777130ad16adcfc28a
Drew Hess <src@drewhess.com>
2021-12-24 11:59:11 +0000

    Buildkite: build all x86_64-linux derivations.

--- nix/buildkite.nix
+++ nix/buildkite.nix
@@ -2,5 +2,9 @@ let
   ciJobs = (import ./flake-compat.nix).defaultNix.ciJobs;
 in
 {
-  inherit (ciJobs.packages.x86_64-linux) tarsnap;
+  amazonImages = ciJobs.amazonImages.x86_64-linux;
+  isoImages = ciJobs.isoImages.x86_64-linux;
+  nixosConfigurations = ciJobs.nixosConfigurations.x86_64-linux;
+  packages = ciJobs.packages.x86_64-linux;
+  tests = ciJobs.tests.x86_64-linux;
 }
