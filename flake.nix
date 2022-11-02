f399bf21f33ae3e13db5a1e7c1266e9ee71e358c
Drew Hess <src@drewhess.com>
2022-11-02 17:36:03 +0000

    chore(nix): Bump dependencies for `openssl-3.0.7` fix.
    We're temporarily bumping to `nixpkgs` main branch to get the
    `openssl-3.0.7` fix, which is not yet included in `nixpkgs-unstable`.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,9 @@
   description = "Hackworth Ltd's nixpkgs overlays and NixOS modules.";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    # Use nixpkgs main branch until openssl-3.0.7 is on -unstable.
+    #nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    nixpkgs.url = github:NixOS/nixpkgs;
     nix-darwin.url = github:LnL7/nix-darwin;
 
     flake-utils.url = github:numtide/flake-utils;
