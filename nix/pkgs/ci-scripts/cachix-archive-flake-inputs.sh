a31ae11f5b36893f9cbc007d5fa97be121183b07
Drew Hess <src@drewhess.com>
2024-02-21 18:07:16 +0000

    chore(ci-scripts): use CACHIX_AUTH_TOKEN rather than config file
    This is a bit more secure than using a config file containing the
    Cachix auth token.
    
    Signed-off-by: Drew Hess <src@drewhess.com>

--- nix/pkgs/ci-scripts/cachix-archive-flake-inputs.sh
+++ nix/pkgs/ci-scripts/cachix-archive-flake-inputs.sh
@@ -2,26 +2,25 @@
 
 usage () {
     program=$(basename "$0")
-    echo "Usage: $program FLAKE CACHIX_CONFIG_PATH CACHIX_CACHE_NAME" >&2
+    echo "Usage: $program FLAKE CACHIX_CACHE_NAME" >&2
     echo >&2
     echo "Archive a Nix flake's inputs to Cachix." >&2
     echo >&2
 }
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 2 ]; then
     usage
     exit 1
 fi
 
 FLAKE="$1"
-CACHIX_CONFIG_PATH="$2"
-CACHIX_CACHE_NAME="$3"
+CACHIX_CACHE_NAME="$2"
 
-if [ ! -f "$CACHIX_CONFIG_PATH" ]; then
-    echo "Cachix config file not found: $CACHIX_CONFIG_PATH" >&2
+if [[ ! -v CACHIX_AUTH_TOKEN ]]; then
+    echo "CACHIX_AUTH_TOKEN environment variable is not set, aborting."
     exit 2
 fi
 
 nix flake archive --json "$FLAKE" \
   | jq -r '.path,(.inputs|to_entries[].value.path)' \
-  | cachix --config "$CACHIX_CONFIG_PATH" push "$CACHIX_CACHE_NAME"
+  | cachix push "$CACHIX_CACHE_NAME"
--- nix/pkgs/ci-scripts/cachix-push-attr.sh
+++ nix/pkgs/ci-scripts/cachix-push-attr.sh
@@ -2,27 +2,26 @@
 
 usage () {
     program=$(basename "$0")
-    echo "Usage: $program ATTR_PATH CACHIX_CONFIG_PATH CACHIX_CACHE_NAME" >&2
+    echo "Usage: $program ATTR_PATH CACHIX_CACHE_NAME" >&2
     echo >&2
     echo "Push build and runtime dependencies of a Nix attribute to Cachix." >&2
     echo >&2
 }
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 2 ]; then
     usage
     exit 1
 fi
 
 ATTR_PATH="$1"
-CACHIX_CONFIG_PATH="$2"
-CACHIX_CACHE_NAME="$3"
+CACHIX_CACHE_NAME="$2"
 
-if [ ! -f "$CACHIX_CONFIG_PATH" ]; then
-    echo "Cachix config file not found: $CACHIX_CONFIG_PATH" >&2
+if [[ ! -v CACHIX_AUTH_TOKEN ]]; then
+    echo "CACHIX_AUTH_TOKEN environment variable is not set, aborting."
     exit 2
 fi
 
 # shellcheck disable=SC2046
 nix-store -qR --include-outputs $(nix-store -qd $(nix-build -A "$ATTR_PATH")) \
  | grep -v '\.drv$' \
- | cachix --config "$CACHIX_CONFIG_PATH" push "$CACHIX_CACHE_NAME"
+ | cachix push "$CACHIX_CACHE_NAME"
