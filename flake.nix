9e658896f5e5787473f3614957a3a95353c457d5
Drew Hess <src@drewhess.com>
2026-04-06 19:49:15 +0100

    chore(nix): bump dependencies
    Also:
    
    - Switch flake inputs to tarball archives rather than Git clones.
    - Retire `nixos-generators` as that's now included in `nixpkgs`
    
    Signed-off-by: Drew Hess <src@drewhess.com>

--- flake.nix
+++ flake.nix
@@ -2,27 +2,24 @@
   description = "Hackworth Ltd Nix.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz";
 
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.url = "https://github.com/nix-darwin/nix-darwin/archive/master.tar.gz";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.url = "https://github.com/NixOS/flake-compat/archive/master.tar.gz";
     flake-compat.flake = false;
 
-    gitignore-nix.url = "github:hercules-ci/gitignore.nix";
+    gitignore-nix.url = "https://github.com/hercules-ci/gitignore.nix/archive/master.tar.gz";
     gitignore-nix.flake = false;
 
-    nixos-generators.url = "github:nix-community/nixos-generators";
-    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
-
-    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.url = "https://github.com/numtide/treefmt-nix/archive/main.tar.gz";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-    pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks-nix.url = "https://github.com/cachix/git-hooks.nix/archive/master.tar.gz";
     pre-commit-hooks-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.url = "https://github.com/hercules-ci/flake-parts/archive/main.tar.gz";
   };
 
   outputs =
@@ -204,10 +201,12 @@
                       # lib.nixosSystem into nixpkgs.lib. We expose it here.
                       inherit (inputs.nixpkgs.lib) nixosSystem;
 
+                      # Ditto for nixosGenerate.
+                      inherit (inputs.nixpkgs.lib) nixosGenerate;
+
                       # Ditto for nix-darwin's lib.darwinSystem function.
                       inherit (inputs.nix-darwin.lib) darwinSystem;
 
-                      inherit (inputs.nixos-generators) nixosGenerate;
                     };
 
                     hacknix = (prev.lib.hacknix or { }) // {
