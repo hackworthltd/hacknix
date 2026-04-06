e108c52c962667f7e432a5d3f8ab94eb9f8d9f84
Drew Hess <src@drewhess.com>
2026-04-06 23:07:41 +0100

    feat: remove remote builder `nix-darwin` modules
    Signed-off-by: Drew Hess <src@drewhess.com>

--- flake.nix
+++ flake.nix
@@ -200,11 +200,6 @@
             default = {
               imports = [
                 ./nix/common/core/module-hashes.nix
-
-                ./nix/darwinModules/config/defaults/default.nix
-                ./nix/darwinModules/config/defaults/nix.nix
-                ./nix/darwinModules/config/remote-builds/build-host
-                ./nix/darwinModules/config/remote-builds/remote-build-host
               ];
               nixpkgs.overlays = [ inputs.self.overlays.default ];
             };
